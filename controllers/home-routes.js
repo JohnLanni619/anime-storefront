d27058b0464c874fee2d3c6c7d2bfc6b9b750dc3
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-07-27 16:32:13 -0400

    added routes and created handlebar files for different pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,46 @@
 const router = require('express').Router();
-const { Category, Product } = require('../models');
+const sequelize = require('../config/connection');
+const { Product } = require('../models');
 
+// get all products for homepage
 router.get('/', (req, res) => {
-  Category.findAll({
-    include: [Product],
+
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'price',
+      'category_id',
+      'img_url',
+    ],
+    // include: [
+    //   {
+    //     model: Comment,
+    //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //     include: {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   },
+    //   {
+    //     model: User,
+    //     attributes: ['username']
+    //   }
+    // ]
   })
-    .then((categories) => res.json(categories))
-    .catch((err) => res.status(500).json(err));
+    .then(dbProductData => {
+      const products = dbProductData.map(products => products.get({ plain: true }));
+
+      res.render('homepage', { 
+        products,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;

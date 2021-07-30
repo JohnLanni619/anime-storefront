d7c9029aea103aebd9bb3ed6fa876b379a3b7d5f
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-07-30 19:19:55 -0400

    Added get, post, and delete routes for comment model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
